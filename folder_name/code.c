2ee70d17bbd61defb7a21ae632c65d4696ae5d17
sonakshi-biswas <biswassonakshi92@gmail.com>
2026-04-05 20:50:40 +0530

    Updated code with input

--- folder_name/code.c
+++ folder_name/code.c
@@ -1,7 +1,10 @@
 #include<stdio.h> 
   
 int main(void) { 
-    printf("Hello, YouTube!\n"); 
+    printf("Hello, YouTube!\n");  
+    printf("what is your name ?");
+    scanf("%49s", name);//read input with a size limit 
+    printf("hello, %s!\n", name); 
     return 0; 
 
 }
